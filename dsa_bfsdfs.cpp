9bc5089b1437d2def5c4fc363aa3246bff94c638
shubhamchitampalle <115708526+shubhamchitampalle@users.noreply.github.com>
2023-06-07 05:18:40 +0530

    Update dsa_bfsdfs.cpp

--- dsa_bfsdfs.cpp
+++ dsa_bfsdfs.cpp
@@ -1,120 +1,189 @@
 #include <iostream>
-#include <stack>
-#include <queue>
-#include <vector>
-#define MAX 10
+#include <stdlib.h>
 using namespace std;
 
-int v;
-int adj_mat[MAX][MAX] = {0};
-vector<vector<int>> adj_list = {{}};
-
-void dfs(int mat[MAX][MAX])
-{
-    bool visited[MAX] = {false};
-    int i = 0;
-    stack<int> s;
-    s.push(i);
-    while (!s.empty())
-    {
-        int t = s.top();
-        if (visited[t] == false)
-        {
-            cout << t << " ";
-        }
-        visited[t] = true;
-        bool flag = false;
-        for (i = 0; i < v; i++)
-        {
-            if (mat[t][i] == 1 && visited[i] == false)
-            {
-                s.push(i);
-                flag = true;
-                break;
-            }
-        }
-        if (!flag)
-        {
-            s.pop();
-        }
-    }
-}
-
-void bfs()
-{
-    bool visited[MAX] = {false};
-    queue<int> temp;
-    temp.push(0);
-    visited[0]=true;
-    while (!temp.empty())
-    {
-        int t = temp.front();
-        cout << t << " ";
-        temp.pop();
-
-        for (int i = 0; i < adj_list[t].size(); i++)
-        {
-            if (visited[adj_list[t][i]] == false)
-            {
-                visited[adj_list[t][i]] = true;
-                temp.push(adj_list[t][i]);
-            }
-        }
-    }
-}
+int cost[10][10], i, j, k, n, qu[10], front, rear, v, visit[10], visited[10];
+int stk[10], top, visit1[10], visited1[10];
 
 int main()
 {
-    cout << "Enter number of vertices:";
+    int m;
+    cout << "Enter number of vertices : ";
+    cin >> n;
+    cout << "Enter number of edges : ";
+    cin >> m;
+
+    cout << "\nEDGES :(u,v)\n";
+    for (k = 1; k <= m; k++)
+    {
+        cout << "\nu:";
+        cin >> i;
+        cout << "v:";
+        cin >> j;
+        cost[i][j] = 1;
+        cost[j][i] = 1;
+    }
+
+    cout << "The adjacency matrix of the graph is:\n";
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cout << " " << cost[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "Enter initial vertex for BFS: ";
+    cin >> v;
+    // Perform BFS
+    cout << "The BFS of the Graph is:\n";
+    front = rear = 0;
+    qu[rear++] = v;
+    visited[v] = 1;
+
+    while (front < rear)
+    {
+        v = qu[front++];
+        cout << v << " ";
+
+        for (j = 1; j <= n; j++)
+        {
+            if (cost[v][j] != 0 && visited[j] == 0)
+            {
+                qu[rear++] = j;
+                visited[j] = 1;
+            }
+        }
+    }
+    cout << endl;
+
+    cout << "Enter initial vertex for DFS: ";
     cin >> v;
 
-    for (int i = 0; i < v; i++)
+    // Perform DFS
+    cout << "The DFS of the Graph is:\n";
+    top = 0;
+    stk[top] = v;
+    visited1[v] = 1;
+    k = 1;
+
+    cout << v << " ";
+    while (top >= 0)
     {
-        adj_list.push_back({});
+        v = stk[top];
+        int flag = 0;
+        for (j = 1; j <= n; j++)
+        {
+            if (cost[v][j] != 0 && visited1[j] == 0)
+            {
+                cout << j << " ";
+                visited1[j] = 1;
+                stk[++top] = j;
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            top--;
     }
 
-    int e;
-    cout << "Enter number of edges:";
-    cin >> e;
-    int i, j;
-    for (int c = 0; c < e; c++)
-    {
-        cout << c;
-        cout << "Enter vertices of Edge " << c+1<< " :" << endl;
-        cout << "Enter vertex 1:";
-        cin >> i;
-        cout << "Enter vertex 2:";
-        cin >> j;
-        adj_mat[i][j] = 1;
-        adj_mat[j][i] = 1;
-        adj_list[i].push_back(j);
-        adj_list[j].push_back(i);
-    }
-    cout << "Adjecency matrix:" << endl;
-    for (int i = 0; i < v; i++)
-    {
-        for (int j = 0; j < v; j++)
-        {
-            cout << adj_mat[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << "Adjecency List:" << endl;
-    for (int i = 0; i < v; i++)
-    {
-        cout << i << "->";
-        for (int j = 0; j < adj_list[i].size(); j++)
-        {
-            cout << adj_list[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    // dfs(adj_mat);
-    cout << "DFS search order is:";
-    dfs(adj_mat);
-    cout << "\nBFS search order is:";
-    bfs();
-    // bfs(adj_mat,v);
     return 0;
 }
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int cost[10][10], i, j, k, n, qu[10], front, rear, v, visit[10], visited[10];
+int stk[10], top, visit1[10], visited1[10];
+
+int main()
+{
+    int m;
+    cout << "Enter number of vertices : ";
+    cin >> n;
+    cout << "Enter number of edges : ";
+    cin >> m;
+
+    cout << "\nEDGES :(u,v)\n";
+    for (k = 1; k <= m; k++)
+    {
+        cout << "\nu:";
+        cin >> i;
+        cout << "v:";
+        cin >> j;
+        cost[i][j] = 1;
+        cost[j][i] = 1;
+    }
+
+    cout << "The adjacency matrix of the graph is:\n";
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cout << " " << cost[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "Enter initial vertex for BFS: ";
+    cin >> v;
+    // Perform BFS
+    cout << "The BFS of the Graph is:\n";
+    front = rear = 0;
+    qu[rear++] = v;
+    visited[v] = 1;
+
+    while (front < rear)
+    {
+        v = qu[front++];
+        cout << v << " ";
+
+        for (j = 1; j <= n; j++)
+        {
+            if (cost[v][j] != 0 && visited[j] == 0)
+            {
+                qu[rear++] = j;
+                visited[j] = 1;
+            }
+        }
+    }
+    cout << endl;
+
+    cout << "Enter initial vertex for DFS: ";
+    cin >> v;
+
+    // Perform DFS
+    cout << "The DFS of the Graph is:\n";
+    top = 0;
+    stk[top] = v;
+    visited1[v] = 1;
+    k = 1;
+
+    cout << v << " ";
+    while (top >= 0)
+    {
+        v = stk[top];
+        int flag = 0;
+        for (j = 1; j <= n; j++)
+        {
+            if (cost[v][j] != 0 && visited1[j] == 0)
+            {
+                cout << j << " ";
+                visited1[j] = 1;
+                stk[++top] = j;
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            top--;
+    }
+
+    return 0;
+}
+
+/*sudo rm -rf /var/log/kern
+sudo rm -rf /var/log/syslog*
+cat /dev/null > ~/.bash_history && history -c*/
